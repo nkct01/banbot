1cd07887220132c8452d451537075dec8b92b68c
ArceusMaster0493 <ArceusMaster0493@users.noreply.github.com>
2016-06-27 22:47:49 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -83,7 +83,7 @@
           type: 'exact',
           functionality: function (chat, cmd) {
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
               var staff = API.getStaff();
               API.sendChat("/me Current staffs online: " + msg.Staff)
