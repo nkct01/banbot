a21bf0ece31176e77c7dc2145f340975eddec0b5
Unbeatableness <unbeatableness@gmail.com>
2016-07-13 15:28:49 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -32,7 +32,8 @@
           functionality: function (chat, cmd) {
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
-            API.sendChat("/me Bacon!!!");
+            else {
+              API.sendChat("/me Bacon!!!");
             }
           }
         };
