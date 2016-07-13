d5e4c7810af5afd87bfba59bb16ee0fdbc10e341
Unbeatableness <unbeatableness@gmail.com>
2016-07-13 15:22:05 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -25,10 +25,23 @@
         }
 
         // Example code for a bot command:
-        bot.commands.mwarCommand = {
-          command: 'mwar',  // The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.baconCommand = {
+          command: 'bacon',  // The command to be called. With the standard command literal this would be: !bacon
           rank: 'user', // Minimum user permission to use the command
           type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("/me Bacon!!!");
+            }
+          }
+        };
+        
+        bot.commands.mwarCommand = {
+          command: 'mwar',
+          rank: 'user',
+          type: 'exact',
           functionality: function (chat, cmd) {
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
@@ -37,7 +50,6 @@
             }
           }
         };
-        
         bot.commands.dyldoCommand = {
           command: 'dyldo',
           rank: 'user',
