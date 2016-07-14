1da9c4baea4e8ea75357befc1254f6126318c310
Unbeatableness <unbeatableness@gmail.com>
2016-07-13 23:04:31 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -59,7 +59,7 @@
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("/me Blacklisted songs: https://github.com/ArceusMaster0493/arkhamBot/blob/master/doc/blacklist.md");
+              API.sendChat("/me Blacklisted songs: https://github.com/smashbro12/arkhamBot/blob/master/doc/blacklist.md");
             }
           }
         };
