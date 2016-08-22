698c7f3ac07fba0970958eb1cad00f90561a6a82
Unbeatableness <unbeatableness@gmail.com>
2016-08-21 20:54:10 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -124,7 +124,7 @@
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("/me Click here for more info on becoming staff in Arkham Network's Plug.dj community: http://arkhamnetwork.org/community/threads/applying-for-plugdj-staff.61770/ ).");
+              API.sendChat("/me Click here for more info on becoming staff in Arkham Network's Plug.dj community: http://arkhamnetwork.org/community/threads/applying-for-plugdj-staff.61770/.");
             }
           }
         },
