bac78b89d536351c7e9b5ac54aa0381a24184891
Unbeatableness <unbeatableness@gmail.com>
2016-08-24 19:51:33 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -228,6 +228,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/smashbro12/arkhamBot/master/SourceCodeEdit", extend);
 
 }).call(this);
