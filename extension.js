ef19e2515f942be780b0b5a2855ee20e4cceffc2
Unbeatableness <unbeatableness@gmail.com>
2016-08-29 19:16:06 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -178,7 +178,7 @@
       botName: "BanBot",
       language: "english",
       chatLink: "https://rawgit.com/smashbro12/arkhamBot/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      scriptLink: "https://rawgit.com/smashbro12/arkhamBot/master/sourcecode.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 200, // 1-200
       startupVolume: 20, // 0-100
@@ -241,6 +241,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/smashbro12/arkhamBot/master/sourcecode.js", extend);
 
 }).call(this);
