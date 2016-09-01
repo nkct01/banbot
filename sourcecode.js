cbfb391279575bf59497381b7866903c930a8955
Unbeatableness <unbeatableness@gmail.com>
2016-08-31 17:51:29 -0700

    Update sourcecode.js

--- sourcecode.js
+++ sourcecode.js
@@ -2855,7 +2855,7 @@
 
             pingCommand: {
                 command: 'ping',
-                rank: 'residentdj',
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
