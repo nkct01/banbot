f93a5f8a5f88aaea9e2093b10ce5a49a63cf0aad
Unbeatableness <unbeatableness@gmail.com>
2016-09-01 19:05:35 -0700

    Update sourcecode.js

--- sourcecode.js
+++ sourcecode.js
@@ -3505,7 +3505,7 @@
 
             usercmdcdCommand: {
                 command: 'usercmdcd',
-                rank: 'manager',
+                rank: 'bouncer',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
