09bd0f62626ec477865e761732b7687a748175a7
Unbeatableness <unbeatableness@gmail.com>
2016-09-01 18:22:22 -0700

    Update sourcecode.js

--- sourcecode.js
+++ sourcecode.js
@@ -2414,7 +2414,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat(subChat(basicBot.chat.invalidtime, {name: chat.un}));
+                        API.sendChat(/me The kick function currently does not work.);
                     }
                 }
             },
