ff7410f9a48e50218aefc0c372f95dede5d6ff70
Unbeatableness <unbeatableness@gmail.com>
2016-09-17 16:57:23 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -188,7 +188,7 @@
       smartSkip: true,
       cmdDeletion: true,
       maximumAfk: 120,
-      afkRemoval: true,
+      afkRemoval: false,
       maximumDc: 120,
       bouncerPlus: true,
       blacklistEnabled: true,
