943235071cac8091aca2b993e3f237240dfbc65a
Unbeatableness <unbeatableness@gmail.com>
2016-09-23 21:44:32 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -154,6 +154,25 @@
             }
           }
         };
+        
+        bot.commands.freezeCommand: = {
+           command: 'freeze',
+           rank: 'manager',
+           type: 'exact',
+           functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+            else {
+                var temp = basicBot.settings.lockdownEnabled;
+                basicBot.settings.lockdownEnabled = !temp;
+                if (basicBot.settings.lockdownEnabled) {
+                    return API.sendChat(subChat(basicBot.chat.toggleon, {name: chat.un, 'function': basicBot.chat.lockdown}));
+                    }
+                    else return API.sendChat(subChat(basicBot.chat.toggleoff, {name: chat.un, 'function': basicBot.chat.lockdown}));
+                }
+            }
+        };
+            
         bot.commands.hammerCommand = {
           command: 'hammer',
           rank: 'bouncer',
